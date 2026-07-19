518b3ea9ba0ca45a7a590e0517f6432e5d46a9eb
tks18 <tksudharshan@gmail.com>
2026-07-19 16:02:06 +0530

    chore(release): 0.1.1

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+# Changelog
+
+All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
+
+### 0.1.1 (2026-07-19)
+
+
+### Others 🔧
+
+* **build:** update pyproject, setup configs for commitlint and changelogs ([532f64a](https://github.com/tks18/personal-finance-etl/commit/532f64acf8dd15ccea391a810d064dd06db854f5))
---
+++ src/__init__.py
@@ -0,0 +1,21 @@
+"""
+Description: 
+Shan's Personal Finance ETL is a tool that automates the process of extracting, 
+transforming, and loading financial data from various sources into a centralized database. 
+It helps users to track their income, expenses, assets, and investments.
+
+Features: 
+- E2E ETL Processing for my Cashflows
+- Portfolio-level calculations
+- Tax calculations
+- Market-calibrated analytics
+- One-click execution
+
+This package contains the source code for the Personal Finance ETL application.
+Author: Sudharshan TK
+"""
+
+__all__ = ["__version__", "PACKAGE_NAME"]
+
+__version__ = "0.1.1"
+PACKAGE_NAME = "personal-finance-etl"
