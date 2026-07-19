3a3bb628abb838290fb6565acdcf33f4d195aa15
tks18 <tksudharshan@gmail.com>
2026-07-19 16:05:15 +0530

    build: add scripts for build and dev tasks

---
+++ scripts/tasks.py
@@ -0,0 +1,12 @@
+import subprocess
+import sys
+
+def dev():
+    """Run the app with hot-reloading."""
+    print("Starting development server...")
+    sys.exit(subprocess.call(["tkreload", "main.py"]))
+
+def build():
+    """Build the executable using PyInstaller."""
+    print("Building executable with PyInstaller...")
+    sys.exit(subprocess.call(["pyinstaller", "--clean", "-y", "main.spec"]))
