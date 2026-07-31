1d99f4dfed0891f3946935d3c7cb0c097669f0b1
tks18 <tksudharshan@gmail.com>
2026-07-31 14:09:48 +0530

    feat(load/schema): rewrite all sqlite3 ddl to duckdb ddl

--- src/load/schema/dimensions.py
+++ src/load/schema/dimensions.py
@@ -2,26 +2,26 @@ DIMENSIONS_DDL = """
 -- DDL: d_Calendar
 CREATE TABLE d_Calendar (
     Date DATE PRIMARY KEY,
-    Day INTEGER,
+    Day BIGINT,
     "Day Name" TEXT,
     "Day Name Short" TEXT,
-    "Day Ordinal" INTEGER,
+    "Day Ordinal" BIGINT,
     "Day Ordinal Name" TEXT,
-    Weekday INTEGER,
-    Week INTEGER,
-    "Week Ordinal" INTEGER,
+    Weekday BIGINT,
+    Week BIGINT,
+    "Week Ordinal" BIGINT,
     "Week Ordinal Name" TEXT,
-    Month INTEGER,
+    Month BIGINT,
     "Month Name" TEXT,
     "Month Name Short" TEXT,
-    Quarter INTEGER,
+    Quarter BIGINT,
     "Quarter Name" TEXT,
-    Year INTEGER,
-    "FY Month" INTEGER,
-    "FY Year" INTEGER,
+    Year BIGINT,
+    "FY Month" BIGINT,
+    "FY Year" BIGINT,
     "Start of Month" DATE,
     "FY Start of Month" DATE,
-    "FY Quarter" INTEGER,
+    "FY Quarter" BIGINT,
     "FY Quarter Name" TEXT,
     "Month - Year" TEXT,
     "Short Month - Year" TEXT,
@@ -40,26 +40,26 @@ CREATE TABLE d_Calendar (
     "Start of Week" DATE,
     "End of Week" DATE,
     "Week Name - Year" TEXT,
-    IS_WEEKEND INTEGER
+    IS_WEEKEND BIGINT
 );
 
 -- DDL: d_Income_Category
 CREATE TABLE d_Income_Category (
-    __file_name__ TEXT, __folder_path__ TEXT, S_NO INTEGER,
-    MODIFY_DATE INTEGER,
+    __file_name__ TEXT, __folder_path__ TEXT, S_NO BIGINT,
+    MODIFY_DATE BIGINT,
     UID TEXT PRIMARY KEY,
     CATEGORY_NAME TEXT,
-    ORDER_SEQUENCE INTEGER,
+    ORDER_SEQUENCE BIGINT,
     CATEGORY_NAME_SHORT TEXT
 );
 
 -- DDL: d_Income_Subcategory
 CREATE TABLE d_Income_Subcategory (
-    __file_name__ TEXT, __folder_path__ TEXT, S_NO INTEGER,
-    MODIFY_DATE INTEGER,
+    __file_name__ TEXT, __folder_path__ TEXT, S_NO BIGINT,
+    MODIFY_DATE BIGINT,
     UID TEXT PRIMARY KEY,
     CATEGORY_NAME TEXT,
-    ORDER_SEQUENCE INTEGER,
+    ORDER_SEQUENCE BIGINT,
     CATEGORY_ID TEXT,
     CATEGORY_GROUPS TEXT,
     FOREIGN KEY (CATEGORY_ID) REFERENCES d_Income_Category(UID)
@@ -67,68 +67,68 @@ CREATE TABLE d_Income_Subcategory (
 
 -- DDL: d_Expense_Category
 CREATE TABLE d_Expense_Category (
-    __file_name__ TEXT, __folder_path__ TEXT, S_NO INTEGER,
-    MODIFY_DATE INTEGER,
+    __file_name__ TEXT, __folder_path__ TEXT, S_NO BIGINT,
+    MODIFY_DATE BIGINT,
     UID TEXT PRIMARY KEY,
     CATEGORY_NAME TEXT,
-    ORDER_SEQUENCE INTEGER
+    ORDER_SEQUENCE BIGINT
 );
 
 -- DDL: d_Expense_Subcategory
 CREATE TABLE d_Expense_Subcategory (
-    __file_name__ TEXT, __folder_path__ TEXT, S_NO INTEGER,
-    MODIFY_DATE INTEGER,
+    __file_name__ TEXT, __folder_path__ TEXT, S_NO BIGINT,
+    MODIFY_DATE BIGINT,
     UID TEXT PRIMARY KEY,
     CATEGORY_NAME TEXT,
-    ORDER_SEQUENCE INTEGER,
+    ORDER_SEQUENCE BIGINT,
     CATEGORY_ID TEXT,
     FOREIGN KEY(CATEGORY_ID) REFERENCES d_Expense_Category(UID)
 );
 
 -- DDL: d_Asset_Category
 CREATE TABLE d_Asset_Category (
-    __file_name__ TEXT, __folder_path__ TEXT, DEVICE_ID INTEGER,
+    __file_name__ TEXT, __folder_path__ TEXT, DEVICE_ID BIGINT,
     UID TEXT PRIMARY KEY,
-    USE_TIME INTEGER,
+    USE_TIME BIGINT,
     ASSET_GROUP TEXT,
-    TYPE INTEGER,
-    ORDER_SEQUENCE INTEGER
+    TYPE BIGINT,
+    ORDER_SEQUENCE BIGINT
 );
 
 -- DDL: d_Asset_Subcategory
 CREATE TABLE d_Asset_Subcategory (
-    __file_name__ TEXT, __folder_path__ TEXT, S_NO INTEGER,
-    CARD_STATEMENT_DATE INTEGER,
-    CARD_PAYMENT_DATE INTEGER,
+    __file_name__ TEXT, __folder_path__ TEXT, S_NO BIGINT,
+    CARD_STATEMENT_DATE BIGINT,
+    CARD_PAYMENT_DATE BIGINT,
     ASSET_NAME TEXT,
-    ORDER_SEQUENCE INTEGER,
+    ORDER_SEQUENCE BIGINT,
     ASSET_DESCRIPTION TEXT,
-    NOTES INTEGER,
-    TRANSFER_EXPENSE INTEGER,
-    CARD_AUTOPAY INTEGER,
-    ADDED_TIME INTEGER,
+    NOTES TEXT,
+    TRANSFER_EXPENSE BIGINT,
+    CARD_AUTOPAY BIGINT,
+    ADDED_TIME BIGINT,
     UID TEXT PRIMARY KEY,
     CURRENCY_ID TEXT,
-    AUTOPAY_ASSET_ID INTEGER,
+    AUTOPAY_ASSET_ID TEXT,
     ASSET_GROUP_ID TEXT,
     FOREIGN KEY(ASSET_GROUP_ID) REFERENCES d_Asset_Category(UID)
 );
 
 -- DDL: d_Currency
 CREATE TABLE d_Currency (
-    __file_name__ TEXT, __folder_path__ TEXT, S_NO INTEGER,
+    __file_name__ TEXT, __folder_path__ TEXT, S_NO BIGINT,
     UID TEXT PRIMARY KEY,
     CURRENCY_NAME TEXT,
     ISO TEXT,
     MAIN_ISO TEXT,
-    ORDER_SEQUENCE INTEGER,
-    RATE REAL,
+    ORDER_SEQUENCE BIGINT,
+    RATE DOUBLE,
     SYMBOL TEXT,
     INSERT_TYPE TEXT,
     SYMBOL_POSITION TEXT,
-    IS_MAIN_CURRENCY INTEGER,
-    IS_SHOW INTEGER,
-    MODIFY_DATE INTEGER,
-    DECIMAL_POINT INTEGER
+    IS_MAIN_CURRENCY BIGINT,
+    IS_SHOW BIGINT,
+    MODIFY_DATE BIGINT,
+    DECIMAL_POINT BIGINT
 );
 """
